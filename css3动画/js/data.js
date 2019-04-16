0c987a299bd388e3610d0fa6592ae67142942cd6
raven <raven@dawoo.com>
2019-04-16 16:47:56 +0800

    raven

---
+++ css3动画/js/data.js
@@ -0,0 +1,25 @@
+//轮播图片
+//doctorId = 描述
+//doctorImg = 路径
+//doctorHref = 链接
+var data_a = {'doctorList_a':[{
+		'doctorId':'d001',
+		'doctorImg':'images/banner_01.jpg',
+		'doctorHref':'baidu.com'
+	},{
+		'doctorId':'d002',
+		'doctorImg':'images/banner_02.jpg',
+		'doctorHref':'javascript:'
+	},{
+		'doctorId':'d003',
+		'doctorImg':'images/banner_03.jpg',
+		'doctorHref':'javascript:'
+	},{
+		'doctorId':'d004',
+		'doctorImg':'images/banner_04.jpg',
+		'doctorHref':'javascript:'
+	},{
+		'doctorId':'d005',
+		'doctorImg':'images/banner_05.jpg',
+		'doctorHref':'javascript:'
+	}]};
